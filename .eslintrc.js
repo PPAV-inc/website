806e4419ca1805038456096f82bf1deb96fa7c3d
xxhomey19 <xxhomey19@gmail.com>
2017-11-27 18:10:36 +0800

    Add root eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['yoctol', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    jasmine: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+    'react/forbid-prop-types': 'off',
+    'react/jsx-wrap-multilines': 'off',
+    'react/jsx-indent': 'off',
+  },
+};
