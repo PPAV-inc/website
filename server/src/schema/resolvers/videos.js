8a09e74e4251c7e3dfbe187742cbcd2f0a25ef34
Ubuntu <ppav@ip-172-31-0-199.ap-southeast-1.compute.internal>
2018-01-01 17:35:11 +0000

    combine mongoDB and ES

--- server/src/schema/resolvers/videos.js
+++ server/src/schema/resolvers/videos.js
@@ -1,4 +1,5 @@
 import subDays from 'date-fns/sub_days';
+import set from 'lodash/set';
 
 import { getMongoDatabase, getElasticsearchDatabase } from '../../database';
 
@@ -8,14 +9,16 @@ export default async (obj, args) => {
     models = [],
     tags = [],
     sources = [],
-    sort,
-    page = 0,
+    // FIXME
+    // sort,
+    // page = 0,
     keyword,
   } = args;
   const db = await getMongoDatabase();
   let results;
 
   const aggregateArr = [];
+  const isPPAV = /^ppav$/i.test(keyword);
 
   if (days > -1) {
     const daysBefore = subDays(new Date(), days);
@@ -30,18 +33,14 @@ export default async (obj, args) => {
   if (sources.length > 0) {
     aggregateArr.push({ $match: { 'videos.source': { $in: sources } } });
   }
-  if (sort) {
-    aggregateArr.push({ $sort: { [sort]: -1 } });
-  }
-  if (page > 0) {
-    aggregateArr.push({ $skip: page * 10 });
-  }
-  if (/^ppav$/i.test(keyword)) {
-    aggregateArr.push({ $sample: { size: 20 } });
-  }
 
-  if (aggregateArr.length > 0) {
-    if (!/^ppav$/i.test(keyword)) {
+  if (aggregateArr.length > 0 || isPPAV) {
+    // aviod too many documents
+    aggregateArr.push({ $limit: 1000 });
+
+    if (isPPAV) {
+      aggregateArr.push({ $sample: { size: 20 } });
+    } else {
       // _id For ES
       aggregateArr.push({ $project: { _id: 1 } });
     }
@@ -52,23 +51,35 @@ export default async (obj, args) => {
       .toArray();
   }
 
-  if (!/^ppav$/i.test(keyword)) {
+  if (!isPPAV) {
     const esClient = getElasticsearchDatabase();
+
+    const queryBody = {
+      query: {
+        bool: {
+          must: {
+            multi_match: {
+              query: keyword,
+              type: 'cross_fields',
+              fields: ['tags^200', 'title^50', 'models^100', 'code^1000'],
+            },
+          },
+        },
+      },
+      min_score: 50,
+      size: 10,
+    };
+
+    if (results) {
+      const filterIds = results.map(each => each._id);
+
+      set(queryBody, 'query.bool.filter.terms._id', filterIds);
+    }
 
     const { hits: { hits } } = await esClient.search({
       index: 'videos',
       type: 'videos',
-      body: {
-        query: {
-          multi_match: {
-            query: keyword,
-            type: 'cross_fields',
-            fields: ['tags^200', 'title^50', 'models^100', 'code^1000'],
-          },
-        },
-        min_score: 50,
-        size: 10,
-      },
+      body: queryBody,
     });
 
     results = hits.map(hit => ({ _id: hit._id, ...hit._source }));
